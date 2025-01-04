47067f718b0c16eb7d3a86fc1fd5a4e537d7f0a8
Your Name <you@example.com>
2025-01-04 22:43:16 +0300

    create to do list

---
+++ main.js
@@ -0,0 +1,69 @@
+const themeDark = document.querySelector(".theme-dark");
+const themeLight = document.querySelector(".theme-light");
+
+themeDark.addEventListener("click", () => {
+  document.body.classList.add("dark");
+});
+
+themeLight.addEventListener("click", () => {
+  document.body.classList.remove("dark");
+});
+
+const Input = document.querySelector(".main__input");
+
+const btnAdd = document.querySelector(".main__btn");
+
+const btnRemoveONE = document.querySelector(".main__btn2");
+
+const btnRemoveALL = document.querySelector(".main__btn3");
+
+const list = document.querySelector(".main__list");
+
+const wrapper = document.querySelector(".main__wrapper");
+
+btnAdd.addEventListener("click", (event) => {
+  const li = document.createElement("li");
+  li.textContent = Input.value;
+  list.appendChild(li);
+  Input.value = "";
+});
+
+btnRemoveONE.addEventListener("click", () => {
+  list.removeChild(list.lastElementChild);
+});
+
+btnRemoveALL.addEventListener("click", () => (list.innerHTML = ""));
+
+const numberTasks = document.querySelector(".header__span");
+
+numberTasks.textContent = list.childElementCount;
+
+btnAdd.addEventListener("click", () => {
+  count = 0;
+  for (let i = 0; i < list.childElementCount; i++) {
+    count++;
+  }
+  numberTasks.textContent = count;
+});
+
+btnRemoveONE.addEventListener("click", () => {
+  count = 0;
+  for (let i = 0; i < list.childElementCount; i++) {
+    count++;
+  }
+  numberTasks.textContent = count;
+});
+
+btnRemoveALL.addEventListener("click", () => {
+  count = 0;
+  for (let i = 0; i < list.childElementCount; i++) {
+    count++;
+  }
+  numberTasks.textContent = count;
+});
+
+wrapper.addEventListener("click", (event) => {
+  if (event.target.tagName === "LI") {
+    event.target.classList.toggle("removeLi");
+  }
+});
